c99ad30c301c3df84fd705356ca22910fff753ad
ivan <ivankohquanwei@gmail.com>
2021-05-25 22:41:06 +0800

    Presentational and Container Components
    - Main is the Container component that has the abstractions from DishDetail and Menu

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import {ListGroup, ListGroupItem} from 'reactstrap';
 
 class DishDetail extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-        }
     }
 
     renderDish(dish) {
@@ -26,12 +25,40 @@ class DishDetail extends Component {
         }
     }
 
+    renderComments(commentsArr) {
+        if (commentsArr == null) { return (<div></div>) }
+        const comments = commentsArr.map((comment) => {
+            return (
+                <>
+                    <ListGroupItem key={comment.id} className="border-0">
+                        {comment.comment}
+                    </ListGroupItem>
+                    <ListGroupItem key={comment.id} className="border-0">
+                        -- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                    </ListGroupItem>
+                </>
+            )
+        })
+        return (
+            <div>
+                <ListGroup>
+                    <h4>Comments</h4>
+                    {comments}
+                </ListGroup>
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div className="col-12 col-md-5 m-1">
-                {this.renderDish(this.props.selectedDish)}
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
+                </div>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderComments(this.props.dish ? this.props.dish.comments : null)}
+                </div>
             </div>
-
         )
     }
 
--- React/confusion/src/components/MenuComponent.js
+++ React/confusion/src/components/MenuComponent.js
@@ -1,51 +1,17 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            selectedDish: null
-        }
-    }
-
-    onDishSelect(dish) {
-        this.setState({selectedDish: dish});
-    }
-
-    renderComments(commentsArr) {
-        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        if (commentsArr == null) { return ( <div></div> )}
-        const comments = commentsArr.map((comment) => {
-            return (
-                <>
-                <ListGroupItem key={comment.id} className="border-0">
-                    {comment.comment}
-                </ListGroupItem>
-                <ListGroupItem key={comment.id} className="border-0">
-                    -- {comment.author} , {months[new Date(comment.date).getMonth()]} {new Date(comment.date).getDate()}, {new Date(comment.date).getFullYear()}
-                </ListGroupItem>
-                </>
-            )
-        })
-        return (
-            <div className="col-12 col-md-5 m-1">
-            <ListGroup> 
-            <h4>Comments</h4>
-                {comments}
-            </ListGroup>
-            </div>
-        )
     }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>                      
                         <CardImgOverlay>
                             <CardTitle>
@@ -58,15 +24,9 @@ class Menu extends Component {
         });
 
         return (
-            <div className="container">
                 <div className="row">
                     {menu}
                 </div>
-                <div className="row">
-                    <DishDetail selectedDish={this.state.selectedDish} />
-                    {this.renderComments(this.state.selectedDish ? this.state.selectedDish.comments : null)}
-                </div>
-            </div>
         );
     }
 }
